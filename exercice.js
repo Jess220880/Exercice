f72b0e957c864ac01968e19d980df1cfe397c002
jess <jess22cohen@gmail.com>
2022-05-12 17:28:36 +0200

    second commit

---
+++ exercice.js
@@ -0,0 +1,8 @@
+alert("Bienvenue");
+
+function activation_bouton(){
+    alert("Bienvenue sur le bouton");
+    
+    console.log("Bouton")
+    alert(document.getElementById("bouton").innerHTML);
+}
